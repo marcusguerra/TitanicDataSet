38f5a7381584bdfd9f4517871f020217b09d749a
marcusguerra <13viniciusmarcus@gmail.com>
2024-02-27 08:32:47 -0300

    ops

--- dataCleanse.py
+++ dataCleanse.py
@@ -10,21 +10,3 @@ dfTraincsv['Title'] = dfTraincsv['Name'].str.extract(r'\b([A-Za-z]+)\.')
 #Miss = mulher não casada
 #Mr = Homem seja casado ou não
 #Outro = outros titulos
-valid_titles = ['Mr', 'Miss', 'Mrs', 'Master']
-dfTraincsv['Title'] = dfTraincsv['Title'].replace({title: 'Outro' for title in dfTraincsv['Title'].unique() if title not in valid_titles})
-
-title_mapping = {'Mr': 0, 'Miss': 1, 'Mrs': 2, 'Master': 3, 'Outro': 4}
-dfTraincsv['Title'] = dfTraincsv['Title'].map(title_mapping)
-
-embarked_mapping = {'S': 0, 'C': 1, 'Q': 2}
-dfTraincsv['Embarked'] = dfTraincsv['Embarked'].map(embarked_mapping)
-
-sex_mapping = {'male': 0, 'female': 1}
-dfTraincsv['Sex'] = dfTraincsv['Sex'].map(sex_mapping)
-
-print(dfTraincsv.columns)
-
-dfRegressao, b0, b1, b2, b3, b4, b5, b6, b7 = returnDf(0.9, dfTraincsv, dfTraincsv)
-dfTraincsv['Survived'] = dfRegressao['Regressao']
-print(dfTraincsv['PassengerId'].head())
-print(dfTraincsv['Survived'].head())
