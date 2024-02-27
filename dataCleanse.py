ce1f6762127b0a7c63799a1fe9b353dd800b7ee8
marcusguerra <13viniciusmarcus@gmail.com>
2024-02-27 08:31:41 -0300

    Train data-set updated

--- dataCleanse.py
+++ dataCleanse.py
@@ -2,32 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-#When we fit a logistic regression model, the intercept term in the model
-# output represent the log odds of the response variable occurring when all
-# predictor variables are equal to zero.
 
-#criar coluna familia
-df = pd.read_csv('train.csv')
-#Criando uma coluna nova com o titulo da pessoa
-df['Title'] = df['Name'].str.extract(r'\b([A-Za-z]+)\.')
+dfTraincsv = pd.read_csv('test.csv')
+dfTraincsv['Title'] = dfTraincsv['Name'].str.extract(r'\b([A-Za-z]+)\.')
 #Master = Homem muito jovem para ser chamado de de Mr
 #Mrs = mulher casada
 #Miss = mulher não casada
 #Mr = Homem seja casado ou não
 #Outro = outros titulos
 valid_titles = ['Mr', 'Miss', 'Mrs', 'Master']
-df['Title'] = df['Title'].replace({title: 'Outro' for title in df['Title'].unique() if title not in valid_titles})
+dfTraincsv['Title'] = dfTraincsv['Title'].replace({title: 'Outro' for title in dfTraincsv['Title'].unique() if title not in valid_titles})
 
-dfmale = df[df['Sex'] == 'male']
-dffemale = df[df['Sex'] == 'female']
+title_mapping = {'Mr': 0, 'Miss': 1, 'Mrs': 2, 'Master': 3, 'Outro': 4}
+dfTraincsv['Title'] = dfTraincsv['Title'].map(title_mapping)
 
-print(len(dffemale['Survived'] == 1))
-def odds(total, acerto):
-    probabilidade = acerto / total
-    odd = probabilidade/(1-probabilidade)
-    return odd
+embarked_mapping = {'S': 0, 'C': 1, 'Q': 2}
+dfTraincsv['Embarked'] = dfTraincsv['Embarked'].map(embarked_mapping)
 
-def calculaBetai(odds1, odds2):
-   divisao = odds1 / odds2
-   return np.log(divisao)
+sex_mapping = {'male': 0, 'female': 1}
+dfTraincsv['Sex'] = dfTraincsv['Sex'].map(sex_mapping)
 
+print(dfTraincsv.columns)
+
+dfRegressao, b0, b1, b2, b3, b4, b5, b6, b7 = returnDf(0.9, dfTraincsv, dfTraincsv)
+dfTraincsv['Survived'] = dfRegressao['Regressao']
+print(dfTraincsv['PassengerId'].head())
+print(dfTraincsv['Survived'].head())
