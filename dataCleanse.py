cd2afc24fdbc4401987c6f6c90368060875ae108
marcusguerra <13viniciusmarcus@gmail.com>
2024-02-27 10:22:07 -0300

    pdf

--- dataCleanse.py
+++ dataCleanse.py
@@ -3,10 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-dfTraincsv = pd.read_csv('test.csv')
-dfTraincsv['Title'] = dfTraincsv['Name'].str.extract(r'\b([A-Za-z]+)\.')
-#Master = Homem muito jovem para ser chamado de de Mr
-#Mrs = mulher casada
-#Miss = mulher não casada
-#Mr = Homem seja casado ou não
-#Outro = outros titulos
+df = pd.read_csv('train.csv')
+
+dfMulher = df[df['Sex'] == 'female']
+total  = dfMulher[dfMulher['Survived'] == 1]
+print(len(total))
+print(len(dfMulher))
+
+def regressaoLogistica(x):
+    exp = np.exp(x)
+    return (exp/(1 + exp))
+
+print(regressaoLogistica(-0.48))
